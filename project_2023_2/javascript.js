41237749bc8f9f97deac53dfe957d4a39a9bfbcc
Keing <0507bercel@gmail.com>
2023-03-26 16:52:13 +0200

    Currency váltó működik

--- project_2023_2/javascript.js
+++ project_2023_2/javascript.js
@@ -1,11 +1,14 @@
 function valtas(){
     var x = document.getElementById("penznem").value;
     var y = document.getElementById("penzkiir").value;
+    var z = document.getElementById("penzkiirfteur").value;
 
     if( x == "EUR"){
-        document.getElementById("penzkiir").innerHTML = Math.floor(y / 380);
+        document.getElementById("penzkiir").innerHTML = Math.floor(y / 400);
+        document.getElementById("penzkiirfteur").innerHTML = "€";
     }
     else if( x == "FT"){
-        document.getElementById("penzkiir").innerHTML = Math.floor(y * 380);
+        document.getElementById("penzkiir").innerHTML = Math.floor(y * 400);
+        document.getElementById("penzkiirfteur").innerHTML = "FT";
     }
 }
