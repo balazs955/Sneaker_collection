eaad99a6099b5ef6d65b7175a8f056630b89d114
Keing <0507bercel@gmail.com>
2023-03-26 17:09:17 +0200

    Currency váltó kész

--- project_2023_2/javascript.js
+++ project_2023_2/javascript.js
@@ -1,7 +1,6 @@
 function valtas(){
     var x = document.getElementById("penznem").value;
     var y = document.getElementById("penzkiir").value;
-    var z = document.getElementById("penzkiirfteur").value;
 
     if( x == "EUR"){
         document.getElementById("penzkiir").innerHTML = Math.floor(y / 400);
@@ -10,5 +9,82 @@ function valtas(){
     else if( x == "FT"){
         document.getElementById("penzkiir").innerHTML = Math.floor(y * 400);
         document.getElementById("penzkiirfteur").innerHTML = "FT";
+    }
+
+    var y2 = document.getElementById("penzkiir2").value;
+
+    if( x == "EUR"){
+        document.getElementById("penzkiir2").innerHTML = Math.floor(y2 / 400);
+        document.getElementById("penzkiirfteur2").innerHTML = "€";
+    }
+    else if( x == "FT"){
+        document.getElementById("penzkiir2").innerHTML = Math.floor(y2 * 400);
+        document.getElementById("penzkiirfteur2").innerHTML = "FT";
+    }
+    
+    var y3 = document.getElementById("penzkiir3").value;
+
+    if( x == "EUR"){
+        document.getElementById("penzkiir3").innerHTML = Math.floor(y3 / 400);
+        document.getElementById("penzkiirfteur3").innerHTML = "€";
+    }
+    else if( x == "FT"){
+        document.getElementById("penzkiir3").innerHTML = Math.floor(y3 * 400);
+        document.getElementById("penzkiirfteur3").innerHTML = "FT";
+    }
+
+    var y4 = document.getElementById("penzkiir4").value;
+
+    if( x == "EUR"){
+        document.getElementById("penzkiir4").innerHTML = Math.floor(y4 / 400);
+        document.getElementById("penzkiirfteur4").innerHTML = "€";
+    }
+    else if( x == "FT"){
+        document.getElementById("penzkiir4").innerHTML = Math.floor(y4 * 400);
+        document.getElementById("penzkiirfteur4").innerHTML = "FT";
+    }
+
+    var y5 = document.getElementById("penzkiir5").value;
+
+    if( x == "EUR"){
+        document.getElementById("penzkiir5").innerHTML = Math.floor(y5 / 400);
+        document.getElementById("penzkiirfteur5").innerHTML = "€";
+    }
+    else if( x == "FT"){
+        document.getElementById("penzkiir5").innerHTML = Math.floor(y5 * 400);
+        document.getElementById("penzkiirfteur5").innerHTML = "FT";
+    }
+
+    var y6 = document.getElementById("penzkiir6").value;
+
+    if( x == "EUR"){
+        document.getElementById("penzkiir6").innerHTML = Math.floor(y6 / 400);
+        document.getElementById("penzkiirfteur6").innerHTML = "€";
+    }
+    else if( x == "FT"){
+        document.getElementById("penzkiir6").innerHTML = Math.floor(y6 * 400);
+        document.getElementById("penzkiirfteur6").innerHTML = "FT";
+    }
+
+    var y7 = document.getElementById("penzkiir7").value;
+
+    if( x == "EUR"){
+        document.getElementById("penzkiir7").innerHTML = Math.floor(y7 / 400);
+        document.getElementById("penzkiirfteur7").innerHTML = "€";
+    }
+    else if( x == "FT"){
+        document.getElementById("penzkiir7").innerHTML = Math.floor(y7 * 400);
+        document.getElementById("penzkiirfteur7").innerHTML = "FT";
+    }
+
+    var y8 = document.getElementById("penzkiir8").value;
+
+    if( x == "EUR"){
+        document.getElementById("penzkiir8").innerHTML = Math.floor(y8 / 400);
+        document.getElementById("penzkiirfteur8").innerHTML = "€";
+    }
+    else if( x == "FT"){
+        document.getElementById("penzkiir8").innerHTML = Math.floor(y8 * 400);
+        document.getElementById("penzkiirfteur8").innerHTML = "FT";
     }
 }
