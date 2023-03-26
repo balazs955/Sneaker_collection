3f41d59a01558cac4cbb4f8618219863d78b8d23
Keing <0507bercel@gmail.com>
2023-03-26 16:40:19 +0200

    Próba képek, currency átváltó

--- project_2023_2/javascript.js
+++ project_2023_2/javascript.js
@@ -1,6 +1,11 @@
-/* function eurhuf(){
-    var huf = document.getElementById("penz").value;
-    var osszeg = huf / 400;
-    document.getElementById("penz").innerHTML =
+function valtas(){
+    var x = document.getElementById("penznem").value;
+    var y = document.getElementById("penzkiir").value;
+
+    if( x == "EUR"){
+        document.getElementById("penzkiir").innerHTML = Math.floor(y / 380);
+    }
+    else if( x == "FT"){
+        document.getElementById("penzkiir").innerHTML = Math.floor(y * 380);
+    }
 }
-*/
